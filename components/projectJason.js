8da449053cad02a47b669ddf4a44639dadd5ec7c
sheheryar92 <38159839+sheheryar92@users.noreply.github.com>
2023-03-15 02:36:15 -0700

    Update projectJason.js

--- components/projectJason.js
+++ components/projectJason.js
@@ -1,18 +1,18 @@
 export const projectData = [
 
-  {
-    title: "react native app",
-    des: "This is react native mobile app using redux , bottom navigation use exo app to scan this QR code to view this app.",
-    source: "/reactNative.png",
-    siteLink: "https://expo.dev/@sheheryarqazi/bundle-dealer-v",
-  },
+
   {
     title: "netflix",
     des: "Netflix colone , react js using , Axios for api call and TMBD Api, Material icons, Firebase hosting.",
     source: "https://cdn.vox-cdn.com/thumbor/vv5WmGOFxeBJkDZKb_L19LGbCjc=/0x0:3000x2000/1400x933/filters:focal(1260x760:1740x1240):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/69608890/ArielDavis_210629_ecl1087_Netflix_0001.0.jpg",
     siteLink: "https://my-netflix-4a7a4.web.app/",
   },
-
+  {
+    title: "react native app",
+    des: "This is react native mobile app using redux , bottom navigation use exo app to scan this QR code to view this app.",
+    source: "https://cdn.dribbble.com/userupload/5362260/file/original-d7b602bd5da42356202c8cbd181d5338.png?compress=1&resize=400x353",
+    siteLink: "https://expo.dev/@sheheryarqazi/bundle-dealer-v",
+  },
   {
     title: "Capricorn Front-end ",
     des: "Ecommerce React Js front End using Third Party Api for produt list. Free hosted on Vercel.",
@@ -30,5 +30,11 @@ export const projectData = [
     des: "Tesla React Js clone , Using Styled Components. Deployed on vercel hosting.",
     source: "https://user-images.githubusercontent.com/62868878/118962083-a16b8f80-b982-11eb-9bf3-3514a3e997f2.png",
     siteLink: "https://tesla-phi.vercel.app/",
+  },
+    {
+    title: "another portfolio",
+    des: "This portfolio built on react js , and Snity for back end Api service , Tailwind Css , Vercel deployment , Axios",
+    source: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw2FhW05mALGI5zANoqmxEGt9B4eR6hSvIyg&usqp=CAU",
+    siteLink: "https://clint-three.vercel.app/",
   },
 ];
